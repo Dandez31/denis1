4f010711e2de0416fa081f26d7fc176c535d01ce
/c <ooodenieska123@gmail.com>
2024-10-02 00:52:01 +0300

    2october

--- 1.7.py
+++ 1.7.py
@@ -6,10 +6,8 @@
 #Напишите программу, которая составляет словарь, используя grades и students, где ключом будет имя ученика, а значением - его средний балл.
 #Вывод в консоль:
 #{'Aaron': 4.0, 'Bilbo': 2.25, 'Johhny': 4.0, 'Khendrik': 3.6666666666666665, 'Steve': 4.8}
-grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
-print(type(grades))
-students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
-print(type(students))
+
+
 
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
